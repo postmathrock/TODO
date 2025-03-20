4beb402b22a71d4ea8cbeb23d6217f0dacb19d80
postmathrock <amamiyarecords@gmail.com>
2025-03-20 19:22:58 +0900

    削除機能の追加

--- main.go
+++ main.go
@@ -16,6 +16,7 @@ type Task struct {
 	TimeLimit   *time.Time `json:"time_limit"`
 	CompletedAt *time.Time `json:"completed_at"`
 	CreatedAt   time.Time  `json:"created_at"`
+	DeletedAt   *time.Time `json:"deleted_at"`
 }
 type TaskToCreate struct {
 	Body      string     `json:"body"`
@@ -36,12 +37,12 @@ func tasks(w http.ResponseWriter, r *http.Request) {
 		log.Fatal(err)
 	}
 
-	rows, err := db.Query("SELECT id, body, time_limit, completed_at, created_at FROM TASK")
+	rows, err := db.Query("SELECT id, body, time_limit, completed_at, created_at, deleted_at FROM TASK")
 
 	var tasks []Task
 	for rows.Next() {
 		var task Task
-		err := rows.Scan(&task.ID, &task.Body, &task.TimeLimit, &task.CompletedAt, &task.CreatedAt)
+		err := rows.Scan(&task.ID, &task.Body, &task.TimeLimit, &task.CompletedAt, &task.CreatedAt, &task.DeletedAt)
 		if err != nil {
 			log.Fatal(err)
 		}
@@ -129,10 +130,36 @@ func update(w http.ResponseWriter, r *http.Request) {
 	db.QueryRow("UPDATE task SET body = $1, time_limit = $2 WHERE id = $3", task.Body, task.TimeLimit, id)
 }
 
+func _delete(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "POSTメソッドのみ許可されています", http.StatusMethodNotAllowed)
+		return
+	}
+	var oi OnlyID
+	if err := json.NewDecoder(r.Body).Decode(&oi); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	defer r.Body.Close()
+
+	db, err := sql.Open("postgres", "host=127.0.0.1 port=5432 user=postgres password=postgres dbname=postgres sslmode=disable")
+	defer db.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// 削除するタスクのID
+	id := oi.ID
+
+	db.QueryRow("UPDATE task SET deleted_at = $1 WHERE id = $2", time.Now(), id)
+
+}
+
 func main() {
 	http.HandleFunc("/api/tasks", tasks)
 	http.HandleFunc("/api/tasks/create", create)
 	http.HandleFunc("/api/tasks/update", update)
 	http.HandleFunc("/api/tasks/complete", complete)
+	http.HandleFunc("/api/tasks/delete", _delete)
 	http.ListenAndServe(":8080", nil)
 }
